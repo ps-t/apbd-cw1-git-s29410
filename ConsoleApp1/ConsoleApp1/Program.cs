4231a4c764a1ae4a49d40fdc6cb6bd8dbd5654f0
TP <s29410@pjwstk.edu.pl>
2026-03-15 11:52:20 -0700

    added input numbers

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,6 +3,12 @@ Console.WriteLine("Input the name:\n");
 string name = Console.ReadLine();
 Console.WriteLine($"Hello, and Welcome {name} to \"THE PROJECT\"");
 
+int[] values = new int[2];
+Console.WriteLine("input first number:");
+values[0] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input second number:");
+values[1] = Convert.ToInt32(Console.ReadLine());
+
 double CalculateAverage(int[] values)
 {
     return values.Average();
