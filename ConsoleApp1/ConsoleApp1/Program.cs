4af45e93100a48bb90aa94cd18cbe0e06489fdad
TP <s29410@pjwstk.edu.pl>
2026-03-15 11:47:23 -0700

    added CalculateAverage method 🧮

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,3 +2,8 @@
 Console.WriteLine("Input the name:\n");
 string name = Console.ReadLine();
 Console.WriteLine($"Hello, and Welcome {name} to \"THE PROJECT\"");
+
+double CalculateAverage(int[] values)
+{
+    return values.Average();
+}
