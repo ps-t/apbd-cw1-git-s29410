9a0dad367de32ac799fb5c25732103275f0a5971
TP <s29410@pjwstk.edu.pl>
2026-03-15 12:38:06 -0700

    added comment at the start

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Hello, This is \"The Project\"");
+﻿// This is the project
+Console.WriteLine("Hello, This is \"The Project\"");
 Console.WriteLine("Input the name:\n");
 string name = Console.ReadLine();
 Console.WriteLine($"Hello, and Welcome {name} to \"THE PROJECT\"");
